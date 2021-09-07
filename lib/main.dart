f9d9a0fabb13a7ee60456d5668fe1ce3ee266356
Daniel Trotter <flare84@gmail.com>
2021-09-06 20:49:57 -0600

    Built out the base of the app. Added AppBar and Body image

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.deepPurple,
+            title: Text("The Set I've Always Wanted"),
+          ),
+          body: Center(
+              child: Image(
+            image: AssetImage('images/lego_podrace.png'),
+          )),
+      ),
+    ),
+  );
 }
